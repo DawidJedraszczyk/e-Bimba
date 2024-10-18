d011ce596a291322101879266111d3e0960fe51c
JerzyJerzu <PJ@pawlik.info.pl>
2024-10-18 17:57:41 +0200

    created benchamrk strategy and implemented one of strategies (uncomplete)

---
+++ benchmark/BenchmarkStrategy.py
@@ -0,0 +1,79 @@
+from abc import abstractmethod
+from datetime import datetime
+from time import time
+import os
+
+from algorithm_parts.utils import time_to_seconds, seconds_to_time, custom_print
+from algorithm_parts.AStarPlanner import AStarPlanner
+
+#from benchmark.routes.automatic_routes import get_all_automatic_sample_routes
+#from benchmark.routes.automatic_routes import get_small_instance_automatic_sample_routes
+
+class BenchmarkStrategy():
+    benchmark_type = None
+    alternative_routes = 3
+    planner_metric = 'straight' # at the end when we will use neural network this will be removed or serve other purpose
+    total_times = []
+    planners = []
+    sample_routes = None
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        self.total_times = []
+        self.planners = []
+        for route in self.sample_routes:
+            start = route.start_cords
+            destination = route.destination_cords
+            start_time = time_to_seconds(route.start_time)
+            start_date = route.date
+            weekday = route.week_day
+
+            planner = AStarPlanner(start_time,start,destination,self.planner_metric,start_date)            
+            total_time = 0
+
+            custom_print(
+                f'Searching for route from: {route.start_name} '
+                f'to: {route.destination_name} '
+                f'at time: {route.start_time} {route.date} '
+                f'({weekday})', 'BENCHMARK')
+            
+            for _ in range(self.alternative_routes):
+                t0 = time()
+                _ = planner.find_next_plan()
+                custom_print(f'(AStarPlannerStraight.find_next_plan = {time()-t0:.4f}s)', 'BENCHMARK')
+                total_time += time()-t0
+
+            self.total_times.append(total_time)
+            self.planners.append(planner)
+
+    def print_found_routes(self):
+        for i, route in enumerate(self.sample_routes):
+            print('##################################################')
+            print('Route from: ', route.start_name, 'to: ', route.destination_name, 'at time: ', route.start_time, ' on: ', route.week_day)
+            print('##################################################')
+            #TODO: print plans as a method in AStarPlanner and implement Plan.__to_string__ method
+            #print_plans(self.planners[i].found_plans)
+            route.print_comparison_plans()
+
+    @abstractmethod
+    def print_results_to_csv(self):
+        pass
+
+    #private methods:
+    def compute_travel_duration(start_time, end_time):
+        start_time_in_seconds = time_to_seconds(start_time)
+        end_time_in_seconds = time_to_seconds(end_time)
+        if end_time_in_seconds < start_time_in_seconds:
+            end_time_in_seconds += time_to_seconds("24:00:00")
+        duration = end_time_in_seconds - start_time_in_seconds
+        return seconds_to_time(duration)
+    
+    def get_csv_filename(self):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        directory = f"benchmark_results/{current_time}"
+        os.makedirs(directory, exist_ok=True)
+        filename = os.path.join(directory, f"{self.benchmark_type}_results{current_time}.csv")
+        return filename
+    
---
+++ benchmark/CustomBenchmark.py
@@ -0,0 +1,46 @@
+import csv
+import os
+
+from benchmark.BenchmarkStrategy import BenchmarkStrategy
+from benchmark.routes_generating.custom_routes import get_custom_sample_routes
+from algorithm_parts.utils import seconds_to_time
+
+class CustomBenchmark(BenchmarkStrategy):
+    def __init__(self):
+        BenchmarkStrategy.__init__(self)
+        self.benchmark_type = 'custom_benchmark'
+        self.sample_routes = get_custom_sample_routes()
+
+    def print_results_to_csv(self):
+        filename = self.get_csv_filename()
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Time searching',
+                            'Start name',
+                            'Destination Name',
+                            'Start Time',
+                            'Day of week',
+                            #'found route',
+                            'jak dojade route',
+                            'google route',
+                            'found route duration',
+                            'jak dojade duration',
+                            'google route duration'
+                            ])
+            for i, route in enumerate(self.sample_routes):
+                writer.writerow([round(self.total_times[i], 3),
+                                route.start_name, 
+                                route.destination_name, 
+                                route.start_time,
+                                route.week_day,
+                                #plans_to_string(planners[i].found_plans), TODO: look at the comment in BenchmarkStrategy
+                                str(route.jakdojade_plan),
+                                str(route.google_plan),
+                                self.compute_travel_duration(
+                                    route.start_time,
+                                    seconds_to_time(self.planners[i].found_plans[0].time_at_destination)),
+                                self.compute_travel_duration(
+                                    route.start_time, 
+                                    route.jakdojade_plan.arrival_time),
+                                self.compute_travel_duration(route.start_time, route.google_plan.arrival_time)
+                                ])
