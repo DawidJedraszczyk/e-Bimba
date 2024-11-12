cb19b7383abc8fb336c3f098c6d233ce520cac35
Jakub Śpiewak <j.m.spiewak@gmail.com>
2024-11-12 16:47:13 +0100

    Fix sample route creation

--- benchmark/routes_generating/automatic_routes.py
+++ benchmark/routes_generating/automatic_routes.py
@@ -7,12 +7,12 @@ def get_all_automatic_sample_routes():
     sample_routes = []
     for start, destination, time, date in product(locations_dict.keys(), locations_dict.keys(), times, dates):
         if start != destination:
-            sample_routes.append(SampleRoute(start, destination, time, date))
+            sample_routes.append(SampleRoute(start, destination, time, date=date))
     return sample_routes
 
 def get_small_instance_automatic_sample_routes():
     sample_routes = []
     for start, destination in product(restricted_locations_dict.keys(), restricted_locations_dict.keys()):
         if start != destination:
-            sample_routes.append(SampleRoute(start, destination, '7:30:00', '2024-09-05'))
+            sample_routes.append(SampleRoute(start, destination, '7:30:00', date='2024-09-05'))
     return sample_routes
