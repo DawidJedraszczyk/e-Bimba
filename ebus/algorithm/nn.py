0fdaafe785e3cb5f73053885ae8c1f89090d0d52
JerzyJerzu <PJ@pawlik.info.pl>
2024-12-03 21:51:02 +0100

    fixed imports and input in nn.py

--- ebus/algorithm/nn.py
+++ ebus/algorithm/nn.py
@@ -1,4 +1,4 @@
-from ai_edge_litert.interpreter import Interpreter
+import time
 import math
 import numpy as np
 from pathlib import Path
@@ -6,22 +6,33 @@ from typing import Callable
 
 from transit.data.misc import INF_TIME, Point
 
+try:
+  from ai_edge_litert.interpreter import Interpreter
+except:
+  from tensorflow.lite.python.interpreter import Interpreter
 
 def load_nn(file: Path) -> Callable[[Point, Point, int, int], int]:
-  interpreter = Interpreter(model_path=str(file))
+  with open(str(file), 'rb') as f:
+    tflite_model = f.read()
+
+  interpreter = Interpreter(model_content=tflite_model)
+  #interpreter = tf.lite.Interpreter(model_path=str(file))
   interpreter.allocate_tensors()
 
   in_idx = interpreter.get_input_details()[0]["index"]
   out_idx = interpreter.get_output_details()[0]["index"]
 
-  def inference(from_point, to_point, day_type, time):
+  def inference(from_point, to_point, day_type, daytime):
+    
     inputs = np.array(
-      [[from_point.x, from_point.y, to_point.x, to_point.y, day_type, time]],
-      dtype=np.float32,
+      [[from_point.x, from_point.y, to_point.x, to_point.y, day_type, daytime]],
+      dtype=np.float64,
     )
 
+    start_t = time.time()
     interpreter.set_tensor(in_idx, inputs)
     interpreter.invoke()
+    forward_pass_time = time.time() - start_t
     output = interpreter.get_tensor(out_idx)[0, 0]
 
     if output < 0:
